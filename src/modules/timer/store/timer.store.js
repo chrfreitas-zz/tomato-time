44f5a4376b0f337dc5fd3f83d7d3f08d6bf07024
Christian Freitas <chrfreitas@gmail.com>
2018-05-07 20:21:59 -0300

    Refactor store

--- src/modules/timer/store/timer.store.js
+++ src/modules/timer/store/timer.store.js
@@ -4,59 +4,65 @@ import { TIMER_DEFAULT, TIMER_TYPES } from '../constants/timer.constant';
 
 let intervalID = 0;
 
-const timerStore = {
-  state: {
-    timer: {
-      ...TIMER_DEFAULT,
-    },
-  },
-  getters: {
-    timerFormatted(state) {
-      const timer = state.timer.seconds;
-      return moment(timer * 1000).format('mm:ss');
-    },
-    isDisabledNext(state) {
-      const lastPosition = TIMER_TYPES.length - 1;
-      return state.timer.position === lastPosition;
-    },
-    isDisabledPrevious(state) {
-      const firstPosition = 0;
-      return state.timer.position === firstPosition;
-    },
-  },
-  mutations: {
-    decrease(state) {
-      state.timer.seconds -= 1;
-    },
-    reset(state) {
-      state.timer = {
-        ...TIMER_DEFAULT,
-      };
-    },
-    changeTimerTo(state, payload) {
-      state.timer = {
-        ...TIMER_TYPES[state.timer.position + payload.index],
-      };
-    },
-  },
-  actions: {
-    play({ commit }) {
-      commit('decrease');
-      intervalID = setInterval(() => {
-        commit('decrease');
-      }, 1000);
-    },
-    pause() {
-      clearInterval(intervalID);
-    },
-    stop({ commit }) {
-      clearInterval(intervalID);
-      commit('reset');
-    },
-    changeTimerTo({ commit }, payload) {
-      commit('changeTimerTo', payload);
-    },
+const stateInitial = {
+  timer: {
+    ...TIMER_DEFAULT,
   },
 };
 
-export default timerStore;
+const getters = {
+  timerFormatted(state) {
+    const timer = state.timer.seconds;
+    return moment(timer * 1000).format('mm:ss');
+  },
+  isDisabledNext(state) {
+    const lastPosition = TIMER_TYPES.length - 1;
+    return state.timer.position === lastPosition;
+  },
+  isDisabledPrevious(state) {
+    const firstPosition = 0;
+    return state.timer.position === firstPosition;
+  },
+};
+
+const mutations = {
+  decrease(state) {
+    state.timer.seconds -= 1;
+  },
+  reset(state) {
+    state.timer = {
+      ...TIMER_DEFAULT,
+    };
+  },
+  changeTimerTo(state, payload) {
+    state.timer = {
+      ...TIMER_TYPES[state.timer.position + payload.index],
+    };
+  },
+};
+
+const actions = {
+  play({ commit }) {
+    commit('decrease');
+    intervalID = setInterval(() => {
+      commit('decrease');
+    }, 1000);
+  },
+  pause() {
+    clearInterval(intervalID);
+  },
+  stop({ commit }) {
+    clearInterval(intervalID);
+    commit('reset');
+  },
+  changeTimerTo({ commit }, payload) {
+    commit('changeTimerTo', payload);
+  },
+};
+
+export default {
+  state: stateInitial,
+  getters,
+  mutations,
+  actions,
+};
