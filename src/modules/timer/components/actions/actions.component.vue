1e883964a800614e561524380017fa6c982a53f9
Christian Freitas <chrfreitas@gmail.com>
2018-05-15 21:38:26 -0300

    Change markup to align button with counters

--- src/modules/timer/components/actions/actions.component.vue
+++ src/modules/timer/components/actions/actions.component.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="actions">
-      <button v-on:click="changeTimerTo(-1)"
-              v-bind:disabled="isDisabledPrevious">Previous</button>
-      <button v-on:click="changeTimerTo(1)"
-              v-bind:disabled="isDisabledNext">Next</button>
+      <button v-on:click="changeTimerTo(-1)" v-bind:disabled="isDisabledPrevious">Previous</button>
+      <counter />
+      <button v-on:click="changeTimerTo(1)" v-bind:disabled="isDisabledNext">Next</button>
     </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 
+import counter from '../counter/counter.component';
+
 export default {
   name: 'Actions',
+  components: {
+    counter,
+  },
   computed: mapGetters([
     'isDisabledNext',
     'isDisabledPrevious',
--- src/modules/timer/timer.component.vue
+++ src/modules/timer/timer.component.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="timer">
-    <counter/>
-    <control/>
     <actions/>
+    <control/>
   </div>
 </template>
 
 <script>
 import control from './components/control/control.component';
-import counter from './components/counter/counter.component';
 import actions from './components/actions/actions.component';
 
 export default {
   name: 'Timer',
   components: {
     control,
-    counter,
     actions,
   },
 };
